6a946e8abcdc738888c29dec9f70dd8f574adb8e
thaflavorsaver <dojohnson@ucdavis.edu>
2023-11-08 16:26:59 -0800

    added base of tilt card to hero

---
+++ app/components/root/hero/Hero.tsx
@@ -0,0 +1,10 @@
+"use client";
+import TiltCard from "@/app/components/root/hero/TiltCard";
+
+export default function Hero() {
+  return (
+    <>
+      <TiltCard />
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 // imports
 "use client";
-
 import NavBar from "@/app/components/shared/NavBar";
+import Hero from "@/app/components/root/hero/Hero";
 
 export default function Home() {
   return (
     <>
       <main>
         <NavBar />
+        <Hero />
       </main>
     </>
   );
