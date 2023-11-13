59297c21e408c75438fbb844413fbbbf74e8f3cd
thaflavorsaver <dojohnson@ucdavis.edu>
2023-11-13 10:52:18 -0800

    commit before doing something stupid

--- app/page.tsx
+++ app/page.tsx
@@ -8,12 +8,22 @@ export default function Home() {
       <main className="min-h-screen bg-white dark:bg-black transition-colors">
         <NavBar />
         <div className="flex flex-col justify-center align-center py-32 text-center">
-          <h1 className="text-black dark:text-white transition-colors font-extrabold text-2xl md:text-4xl lg:text-6xl xl:text-8xl">
-            Hi, I'm a full-stack <br />
-            <span className="animated-gradient font-extrabold text-3xl md:text-5xl lg:text-7xl xl:text-9xl">
-              Problem Solver
-            </span>
-          </h1>
+          <motion.h1
+            initial={{ opacity: 0, y: -25, scale: 0.5 }}
+            animate={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ type: "spring", stiffness: 260, damping: 20 }}
+            className="text-black dark:text-white transition-colors font-extrabold text-3xl md:text-5xl lg:text-7xl xl:text-9xl"
+          >
+            I'm a full-stack <br />
+          </motion.h1>
+          <motion.h1
+            initial={{ opacity: 0, y: 25, scale: 0.5 }}
+            animate={{ opacity: 1, y: 0, scale: 1 }}
+            transition={{ type: "spring", stiffness: 260, damping: 20 }}
+            className="animated-gradient font-extrabold text-3xl md:text-5xl lg:text-7xl xl:text-9xl"
+          >
+            Problem Solver
+          </motion.h1>
         </div>
       </main>
     </>
