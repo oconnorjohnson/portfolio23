71efaf8cef9208a96b10baa9b88b4c39f0d80eba
thaflavorsaver <dojohnson@ucdavis.edu>
2023-11-12 19:33:44 -0800

    projects are light/dark responsive

--- app/components/projects/Projects.tsx
+++ app/components/projects/Projects.tsx
@@ -4,29 +4,24 @@ import { motion } from "framer-motion";
 
 export default function BouncyCardsFeatures() {
   return (
-    <section className="mx-auto max-w-7xl px-4 py-12 text-slate-800">
+    <section className="mx-auto max-w-7xl px-4 py-12 text-gray-800 dark:text-gray-200 transition-colors">
       <div className="mb-8 flex flex-col items-start justify-between gap-4 md:flex-row md:items-end md:px-8">
         <h2 className="max-w-lg text-4xl font-bold md:text-5xl">
           Grow faster with our
-          <span className="text-slate-400"> all in one solution</span>
+          <span className="text-slate-400 dark:text-white transition-colors">
+            {" "}
+            all in one solution
+          </span>
         </h2>
         <motion.button
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
-          className="whitespace-nowrap rounded-lg bg-slate-900 px-4 py-2 font-medium text-white shadow-xl transition-colors hover:bg-slate-700"
+          className="whitespace-nowrap rounded-3xl bg-gray-800 dark:bg-gray-200 px-4 py-2 font-medium text-gray-200 dark:text-gray-800 shadow-xl transition-colors hover:bg-slate-700"
         >
           Learn more
         </motion.button>
       </div>
       <div className="mb-4 grid grid-cols-12 gap-4">
-        <BounceCard className="col-span-12 md:col-span-4">
-          <CardTitle>Do a thing</CardTitle>
-          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
-            <span className="block text-center font-semibold text-indigo-50">
-              FEATURE DEMO HERE
-            </span>
-          </div>
-        </BounceCard>
         <BounceCard className="col-span-12 md:col-span-8">
           <CardTitle>Do another thing</CardTitle>
           <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-amber-400 to-orange-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
@@ -35,8 +30,16 @@ export default function BouncyCardsFeatures() {
             </span>
           </div>
         </BounceCard>
+        <BounceCard className="col-span-12 md:col-span-4">
+          <CardTitle>Do a thing</CardTitle>
+          <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-violet-400 to-indigo-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
+            <span className="block text-center font-semibold text-indigo-50">
+              FEATURE DEMO HERE
+            </span>
+          </div>
+        </BounceCard>
       </div>
-      <div className="grid grid-cols-12 gap-4">
+      {/* <div className="grid grid-cols-12 gap-4">
         <BounceCard className="col-span-12 md:col-span-8">
           <CardTitle>And this too</CardTitle>
           <div className="absolute bottom-0 left-4 right-4 top-32 translate-y-8 rounded-t-2xl bg-gradient-to-br from-green-400 to-emerald-400 p-4 transition-transform duration-[250ms] group-hover:translate-y-4 group-hover:rotate-[2deg]">
@@ -53,7 +56,7 @@ export default function BouncyCardsFeatures() {
             </span>
           </div>
         </BounceCard>
-      </div>
+      </div> */}
     </section>
   );
 }
@@ -68,7 +71,7 @@ const BounceCard = ({
   return (
     <motion.div
       whileHover={{ scale: 0.95, rotate: "-1deg" }}
-      className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-slate-100 p-8 ${className}`}
+      className={`group relative min-h-[300px] cursor-pointer overflow-hidden rounded-2xl bg-gray-200 dark:bg-gray-400 transition-colors p-8 ${className}`}
     >
       {children}
     </motion.div>
@@ -77,6 +80,8 @@ const BounceCard = ({
 
 const CardTitle = ({ children }: { children: ReactNode }) => {
   return (
-    <h3 className="mx-auto text-center text-3xl font-semibold">{children}</h3>
+    <h3 className="mx-auto text-center text-3xl text-gray-800 dark:text-black transition-colors font-semibold">
+      {children}
+    </h3>
   );
 };
