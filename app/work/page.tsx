a067a7c450d9397aa4645ded3e6e1ed4babca797
thaflavorsaver <dojohnson@ucdavis.edu>
2023-11-12 18:47:26 -0800

    added base of /work in /projects/projects component

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,10 +1,12 @@
 import NavBar from "@/app/components/shared/NavBar";
+import Projects from "@/app/components/projects/Projects";
 
 export default function Work() {
   return (
     <>
       <div className="min-h-screen bg-white dark:bg-black transition-colors">
         <NavBar />
+        <Projects />
       </div>
     </>
   );
